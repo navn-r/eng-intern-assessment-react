3b2856f602efc3f402ea049716ebf4c1354b1bde
Navinn Ravindaran <navinn@duck.com>
2024-01-14 19:33:41 -0500

    fix: update breaking tests

--- __tests__/Stopwatch.test.js
+++ __tests__/Stopwatch.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import Stopwatch from '../src/StopWatch';
 import '@testing-library/jest-dom';
 
@@ -7,42 +7,51 @@ describe('Stopwatch', () => {
   test('renders initial state correctly', () => {
     render(<Stopwatch />);
 
-    expect(screen.getByText('00:00:00')).toBeInTheDocument();
+    expect(screen.getByTestId('display').textContent).toEqual('00:00:00:000');
     expect(screen.queryByTestId('lap-list')).toBeEmptyDOMElement();
   });
 
-  test('starts and stops the stopwatch', () => {
+  test('starts and stops the stopwatch', async () => {
     render(<Stopwatch />);
 
     fireEvent.click(screen.getByText('Start'));
-    expect(screen.getByText(/(\d{2}:){2}\d{2}/)).toBeInTheDocument();
+    expect(screen.getByTestId('display')).toBeInTheDocument();
+    expect(screen.getByTestId('display').textContent).toMatch(
+      /(\d{2}:){2}\d{2}/
+    );
 
     fireEvent.click(screen.getByText('Stop'));
-    expect(screen.queryByText(/(\d{2}:){2}\d{2}/)).not.toBeInTheDocument();
+    const pausedTime = screen.getByTestId('display').textContent;
+
+    await act(() => new Promise((r) => setTimeout(r, 10)));
+    expect(screen.getByTestId('display').textContent).toBe(pausedTime);
   });
 
-  test('pauses and resumes the stopwatch', () => {
+  test('pauses and resumes the stopwatch', async () => {
     render(<Stopwatch />);
 
     fireEvent.click(screen.getByText('Start'));
-    fireEvent.click(screen.getByText('Pause'));
-    const pausedTime = screen.getByText(/(\d{2}:){2}\d{2}/).textContent;
+    fireEvent.click(screen.getByText('Stop'));
+    const pausedTime = screen.getByTestId('display').textContent;
 
-    fireEvent.click(screen.getByText('Resume'));
-    expect(screen.getByText(/(\d{2}:){2}\d{2}/).textContent).not.toBe(
-      pausedTime
-    );
+    fireEvent.click(screen.getByText('Start'));
+    await act(() => new Promise((r) => setTimeout(r, 10)));
+    expect(screen.getByTestId('display').textContent).not.toBe(pausedTime);
   });
 
-  test('records and displays lap times', () => {
+  test('records and displays lap times', async () => {
     render(<Stopwatch />);
 
     fireEvent.click(screen.getByText('Start'));
+    await act(() => new Promise((r) => setTimeout(r, 10)));
+
     fireEvent.click(screen.getByText('Lap'));
-    expect(screen.getByTestId('lap-list')).toContainElement(
-      screen.getByText(/(\d{2}:){2}\d{2}/)
+    expect(screen.getByTestId('lap-list').children.length).toBe(1);
+    expect(screen.getByTestId('lap-list').children[0].textContent).toMatch(
+      /(\d{2}:){2}\d{2}/
     );
 
+    await act(() => new Promise((r) => setTimeout(r, 10)));
     fireEvent.click(screen.getByText('Lap'));
     expect(screen.getByTestId('lap-list').children.length).toBe(2);
   });
@@ -54,7 +63,7 @@ describe('Stopwatch', () => {
     fireEvent.click(screen.getByText('Lap'));
     fireEvent.click(screen.getByText('Reset'));
 
-    expect(screen.getByText('00:00:00')).toBeInTheDocument();
+    expect(screen.getByTestId('display').textContent).toEqual('00:00:00:000');
     expect(screen.queryByTestId('lap-list')).toBeEmptyDOMElement();
   });
 });
